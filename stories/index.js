0f9cf4821d02a87664ce2f4eaca7f4682dc10494
Rachel PR <rplourder@gmail.com>
2022-05-15 16:48:49 -0300

    Add import for DayListItem.js

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,8 @@ import "index.scss";
 
 import Button from "components/Button";
 
+import DayListItem from "components/DayListItem"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
