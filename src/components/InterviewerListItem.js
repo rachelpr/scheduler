2ab13d51acd1af1769e71a19873124cf1cd941c0
Rachel PR <rplourder@gmail.com>
2022-05-16 22:34:55 -0300

    Build out InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li
+      className={interviewerClass}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
