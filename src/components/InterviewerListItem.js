4e6a8f7df1ef5e11e2a9838e89e379096e9ea57b
Rachel PR <rplourder@gmail.com>
2022-05-17 21:44:51 -0300

    Fix avatar and alt to render dynamically

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -16,8 +16,8 @@ export default function InterviewerListItem(props) {
     >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
       {props.selected && props.name}
     </li>
