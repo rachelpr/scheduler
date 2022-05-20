0b69cbfd64cb3d33a830788d2b3d8a3d0271301b
Rachel PR <rplourder@gmail.com>
2022-05-20 16:29:45 -0300

    Add in various appointment componenets

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "components/Appointment/styles.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import Confirm from "./Confirm";
+import Status from "./Status";
 
 export default function Appointment(props) {
   return <article className="appointment"></article>;
