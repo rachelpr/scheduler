d1ba20fab0ee145b8bcf160b8c963e4e0c6cbc3e
Rachel PR <rplourder@gmail.com>
2022-05-23 12:14:07 -0300

    Add in axios and useEffect. Add functinality to get days from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -42,27 +43,15 @@ const appointments = {
   },
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      setDays(response.data);
+    });
+  }, []);
   const appointmentList = Object.values(appointments).map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment}/>;
+    return <Appointment key={appointment.id} {...appointment} />;
   });
   return (
     <main className="layout">
@@ -74,7 +63,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={days} value={days} onChange={setDays} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
