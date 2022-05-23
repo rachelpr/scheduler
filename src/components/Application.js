93f5e4bb16a9ae38ff59c782b8c9fa8089da2839
Rachel PR <rplourder@gmail.com>
2022-05-23 15:02:26 -0300

    Add state object and helper function to manage state for the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,52 +4,59 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
-
 export default function Application(props) {
-  const [days, setDays] = useState([]);
+  const appointments = {
+    "1": {
+      id: 1,
+      time: "12pm",
+    },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer:{
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+    },
+    "4": {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer:{
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        }
+      }
+    },
+    "5": {
+      id: 5,
+      time: "4pm",
+    }
+  };
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+  const setDay = (day) => setState({ ...state, day });
+
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+
   useEffect(() => {
     axios.get("http://localhost:8001/api/days").then((response) => {
       setDays(response.data);
     });
   }, []);
+
   const appointmentList = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
@@ -63,7 +70,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={days} onChange={setDays} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
