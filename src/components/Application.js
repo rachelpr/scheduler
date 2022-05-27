cda2bff1710c24508f980503d4383eb9be385a9e
Rachel PR <rplourder@gmail.com>
2022-05-27 15:18:11 -0300

    Import getinterviewersforday function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,47 +3,9 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
-  // const appointments = {
-  //   "1": {
-  //     id: 1,
-  //     time: "12pm",
-  //   },
-  //   "2": {
-  //     id: 2,
-  //     time: "1pm",
-  //     interview: {
-  //       student: "Lydia Miller-Jones",
-  //       interviewer:{
-  //         id: 3,
-  //         name: "Sylvia Palmer",
-  //         avatar: "https://i.imgur.com/LpaY82x.png",
-  //       }
-  //     }
-  //   },
-  //   "3": {
-  //     id: 3,
-  //     time: "2pm",
-  //   },
-  //   "4": {
-  //     id: 4,
-  //     time: "3pm",
-  //     interview: {
-  //       student: "Archie Andrews",
-  //       interviewer:{
-  //         id: 4,
-  //         name: "Cohana Roy",
-  //         avatar: "https://i.imgur.com/FK8V841.jpg",
-  //       }
-  //     }
-  //   },
-  //   "5": {
-  //     id: 5,
-  //     time: "4pm",
-  //   }
-  // };
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -51,8 +13,8 @@ export default function Application(props) {
     interviewers: {},
   });
   const setDay = (day) => setState({ ...state, day });
-  //const setDays = (days) => setState((prev) => ({ ...prev, days }));
   const dailyAppointments = getAppointmentsForDay(state,state.day);
+  const dailyInterviewers = getInterviewersForDay(state,state.day)
   const daysEndpoint = "http://localhost:8001/api/days";
   const apptsEndpoint = "http://localhost:8001/api/appointments";
   const interviewersEndpoint = "http://localhost:8001/api/interviewers";
