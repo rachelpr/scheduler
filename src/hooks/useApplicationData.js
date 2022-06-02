1baea85ab1c303382dab4f28540f83c468a295b7
Rachel PR <rplourder@gmail.com>
2022-06-02 20:59:26 -0300

    Add in function to count spots, get updated days aray and set the spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function useApplicationData() {
   const [state, setState] = useState({
@@ -8,7 +9,7 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {},
   });
-
+  //console.log(state);
   const setDay = (day) => setState({ ...state, day });
 
   const bookInterview = function (id, interview) {
@@ -22,13 +23,38 @@ export default function useApplicationData() {
       [id]: appointment,
     };
 
-    //console.log(interview)
     return axios.put(`${apptsEndpoint}${id}`, { interview }).then(() => {
-      setState({
+      // getting the new days array to be updated 
+      // passing a snapshot of state/appointments
+      const updatedDays = getUpdatedDaysArray({
         ...state,
         appointments,
       });
+      setState({
+        ...state,
+        appointments,
+        days: [...updatedDays],
+      });
     });
+  };
+
+  const getUpdatedDaysArray = function (newState) {
+
+    //search for the day in state
+    const foundDay = newState.days.find((day) => day.name === newState.day);
+    // update the foundDay.spots with the spots remaining
+    
+    foundDay.spots = countSpots(newState);
+    // getting the index of the day in newState
+    const foundDayIndex = newState.days.findIndex(
+      (day) => day.name === newState.day
+    );
+    // create a copy of the days array
+    const daysCopy = [...newState.days];
+    //update foundDay in daysCopy
+    daysCopy[foundDayIndex] = foundDay;
+    // setting the newState with the updated copy of the days array
+    return daysCopy;
   };
 
   const cancelInterview = function (id) {
@@ -40,12 +66,31 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+    console.log(appointments);
     return axios.delete(`${apptsEndpoint}${id}`).then(() => {
-      setState({
+      const updatedDays = getUpdatedDaysArray({
         ...state,
         appointments,
       });
+      setState({
+        ...state,
+        appointments,
+        days: [...updatedDays],
+      });
     });
+  };
+
+  const countSpots = function (newState) {
+    let spotsFree = 0;
+    //use getAppointmentsForDay to get the appointments array
+    //loop through appointments array (in day)
+    for (let appointment of getAppointmentsForDay(newState, newState.day)) {
+      //check if the interview has a null value
+      if (appointment.interview === null) {
+        spotsFree += 1;
+      }
+    }
+    return spotsFree;
   };
 
   // various API endpoints
@@ -72,5 +117,5 @@ export default function useApplicationData() {
     //setDays(response.data);
   }, []);
 
-  return { state, setDay, bookInterview, cancelInterview };
+  return { state, setDay, bookInterview, cancelInterview, countSpots };
 }
