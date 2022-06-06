3998127a0befbf36b2752c2cb7545fefc87d8e81
Rachel PR <rplourder@gmail.com>
2022-06-06 20:17:03 -0300

    Add in tests for the form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -7,6 +7,7 @@ import {
   fireEvent,
   getByText,
   queryByText,
+  getAllByAltText,
 } from "@testing-library/react";
 import Form from "components/Appointment/Form";
 
@@ -67,24 +68,89 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name and interviewer is defined", () => {
-    /* 1. Create the mock onSave function */
+  // it("calls onSave function when the name and interviewer is defined", () => {
+  //   /* 1. Create the mock onSave function */
+  //   const onSave = jest.fn();
+  //   /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+  //   const { getByText, queryByText } = render(
+  //     <Form
+  //       interviewers={interviewers}
+  //       student="Lydia Miller-Jones"
+  //       onSave={() => onSave("Lydia Miller-Jones", 1)}
+  //       interviewer={interviewers[0]}
+  //     />
+  //   );
+  //   /* 3. Click the save button */
+  //   fireEvent.click(getByText("Save"));
+
+  //   expect(queryByText(/student name cannot be blank/i)).toBeNull();
+  //   expect(queryByText(/please select an interviewer/i)).toBeNull();
+  //   expect(onSave).toHaveBeenCalledTimes(1);
+  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  // });
+  xit("submits the name entered by the user", () => {
     const onSave = jest.fn();
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    const { getByText, queryByText } = render(
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} interviewer={interviewers[0]} />
+    );
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", interviewers[0]);
+  });
+  it("can successfully save after trying to submit an empty student name", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
       <Form
         interviewers={interviewers}
-        student="Lydia Miller-Jones"
-        onSave={()=>onSave("Lydia Miller-Jones", 1)}
+        onSave={onSave}
         interviewer={interviewers[0]}
       />
     );
-    /* 3. Click the save button */
+
+    fireEvent.click(getByText("Save"));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
+
+    
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", interviewers[0]);
+  });
+  it("calls onCancel and resets the input field", () => {
+    const onCancel = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        name="Lydia Mill-Jones"
+        onSave={jest.fn()}
+        onCancel={onCancel}
+      />
+    );
+
+    fireEvent.click(getByText("Save"));
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
