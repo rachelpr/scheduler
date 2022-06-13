1a4324e4924f12c8f4b56af3115273d9b40300c8
Rachel PR <rplourder@gmail.com>
2022-06-13 20:52:27 -0300

    Removing commented code

--- stories/index.js
+++ stories/index.js
@@ -1,34 +1,19 @@
 import React, { Fragment } from "react";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
-
 import Button from "components/Button";
-
 import DayListItem from "components/DayListItem";
-
 import DayList from "components/DayList";
-
 import InterviewerListItem from "components/InterviewerListItem";
-
 import InterviewerList from "components/InterviewerList";
-
 import Appointment from "components/Appointment/index.js";
-
 import Header from "components/Appointment/Header";
-
 import Empty from "components/Appointment/Empty";
-
 import Show from "components/Appointment/Show";
-
 import Confirm from "components/Appointment/Confirm";
-
 import Status from "components/Appointment/Status";
-
 import Error from "components/Appointment/Error";
-
 import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
