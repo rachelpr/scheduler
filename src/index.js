5226b4bc0f069f55b17a30575a0b11bf2919748c
Rachel PR <rplourder@gmail.com>
2022-10-31 16:51:29 -0300

    Add files for netlify deployment

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import "index.scss";
 
 import Application from "components/Application";
 
+import axios from "axios";
+
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
